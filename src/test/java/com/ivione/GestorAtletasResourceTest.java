7bbd88a3d4804206d554bc12ea9a7b1d9d70b487
Iván García <ivgarciag@yahoo.es>
2020-08-10 12:23:58 +0200

    Test comentados

--- src/test/java/com/ivione/GestorAtletasResourceTest.java
+++ src/test/java/com/ivione/GestorAtletasResourceTest.java
@@ -1,9 +1,5 @@
 package com.ivione;
 
-import static io.restassured.RestAssured.given;
-
-import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.h2.H2DatabaseTestResource;
@@ -11,13 +7,5 @@ import io.quarkus.test.h2.H2DatabaseTestResource;
 @QuarkusTestResource(H2DatabaseTestResource.class)
 @QuarkusTest
 public class GestorAtletasResourceTest {
-
-    @Test
-    public void testAmbitosEndpoint() {
-        given()
-          .when().get("/gestorAtletas/ambitos")
-          .then()
-             .statusCode(200);
-    }
 
 }
